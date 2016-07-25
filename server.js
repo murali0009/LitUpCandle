54e0b538fbe8cc4b75a6f3a70b5294a74542565f
Muralidharan <nandam@ncs.com.sg>
2016-07-25 10:27:41 +0800

    Commented Sequelize

--- server.js
+++ server.js
@@ -3,23 +3,23 @@
 var express = require("express");
 var app = express();
 var http = require("http");
-var Sequelize = require('Sequelize');
+//var Sequelize = require('Sequelize');
 
 app.use(express.static('public'));
 
-var sequelize = new Sequelize('litupcandle', 'dblitupcandleadmin', 'l1tupc@ndle', {
-	host: 'dblitupcandle.database.windows.net',
-	dialect: 'mssql'
-});
+// var sequelize = new Sequelize('litupcandle', 'dblitupcandleadmin', 'l1tupc@ndle', {
+// 	host: 'dblitupcandle.database.windows.net',
+// 	dialect: 'mssql'
+// });
 
-sequelize
-	.authenticate()
-	.then(function(err) {
-		console.log('Connection has been established successfully.');
-	})
-	.catch(function (err) {
-		console.log('Unable to connect to the database:', err);
-	});
+// sequelize
+// 	.authenticate()
+// 	.then(function(err) {
+// 		console.log('Connection has been established successfully.');
+// 	})
+// 	.catch(function (err) {
+// 		console.log('Unable to connect to the database:', err);
+// 	});
 
 var server = http.createServer(app);
 
